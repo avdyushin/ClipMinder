feca473a4ac49df0e9f572021e0527ecdcc4f705
Grigory Avdyushin <avdyushin.g@gmail.com>
2025-02-03 14:26:14 +0100

    Initial Commit

---
+++ ClipMinder/ClipMinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipMinderApp.swift
+//  ClipMinder
+//
+//  Created by Grigory Avdyushin   on 03/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ClipMinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipMinderTests/ClipMinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipMinderTests.swift
+//  ClipMinderTests
+//
+//  Created by Grigory Avdyushin   on 03/02/2025.
+//
+
+import Testing
+@testable import ClipMinder
+
+struct ClipMinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
